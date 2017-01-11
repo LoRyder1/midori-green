7836f848cd26482cf736e464dbe202e068223308
Lovinder Pnag <lspnag@gmail.com>
2017-01-11 08:10:20 -0800

    get initial state

---
+++ app/assets/javascripts/components/horses.js.jsx
@@ -0,0 +1,13 @@
+this.Horses = React.createClass({
+  getInitialState: function() {
+    return {
+      horses: this.props.data
+    };
+  },
+
+  render: function() {
+    return (
+      
+    )
+  }
+})
