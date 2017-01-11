f19e2801f306946d229c246f0467c94a8854da66
Lovinder Pnag <lspnag@gmail.com>
2017-01-11 08:18:33 -0800

    render table

--- app/assets/javascripts/components/horses.js.jsx
+++ app/assets/javascripts/components/horses.js.jsx
@@ -7,7 +7,24 @@ this.Horses = React.createClass({
 
   render: function() {
     return (
-      
+      <div className='horses'>
+        <h2 className='name'>Horses</h2>
+        <div className='row'></div>
+        <HorseForm handleNewHorse={this.addHorse} />
+        <hr></hr>
+        <table className='table table-bordered'>
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Breed</th>
+            <th>Age</th>
+            <th>Actions</th>
+          </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
     )
   }
 })
